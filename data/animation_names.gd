5971339248a15633e213b701aaa88cc0b3aca28a
Cristian3016 <cristiancaro163@gmail.com>
2026-02-06 16:06:01 -0300

    Nuevos Cambios

--- data/animation_names.gd
+++ data/animation_names.gd
@@ -3,3 +3,4 @@ extends Resource
 
 @export var death : StringName = "death"
 @export var hit   : StringName = "hit"
+@export var walk  : StringName ="walk"
--- objects/characters/wander_action.gd
+++ objects/characters/wander_action.gd
@@ -1,21 +1,40 @@
 class_name WanterAction
 extends ActionLeaf
 ## Randoly move character left and right after as set timer
-@export var timer : Timer
+@export var timer      : Timer
+@export var sprite     : AnimatedSprite2D
+@export var animations : AnimationNames
+@export var keys       : BlackboardKeys
 
 @onready var random_gen = RandomNumberGenerator.new()
 var character : PlatformerCharacter2D
+var is_wandering = false
+
+func after_run(actor: Node, blackboard: Blackboard) -> void:
+	is_wandering = false
+	timer.timeout.disconnect(_on_timer_timeout)
 
 func before_run(p_actor: Node, blackboard: Blackboard) -> void:
 	character= p_actor as PlatformerCharacter2D
+	sprite.play(animations.walk)
 	timer.timeout.connect(_on_timer_timeout)
 	timer.start()
 	randomizer_wandering()
 
-func tick(p_actor: Node, blackboard: Blackboard) -> int:
-		return RUNNING
+func tick(p_actor: Node, p_blackboard: Blackboard) -> int:
+	var is_alive = p_blackboard.get_value(keys.is_alive)
+	var is_hit = p_blackboard.get_value(keys.is_hit)
+	
+	if is_alive == false || is_hit == true :
+		return FAILURE
 		
+	if is_wandering:
+		return RUNNING
+	
+	return SUCCESS
+				
 func randomizer_wandering():
+	is_wandering = true 
 	var randi = random_gen.randi()
 	
 	if randi % 2 == 1:
@@ -26,5 +45,5 @@ func randomizer_wandering():
 	character.velocity = character.direction * character.stats.walk_speed
 		
 func _on_timer_timeout():
-	randomizer_wandering() 
-	timer.start()
+	is_wandering = false
+	
