8b9d65db62dc2acb172ca09b593491931982adc9
Cristian3016 <cristiancaro163@gmail.com>
2026-03-05 21:21:48 -0300

    Sound Level, Main, Intro and Button

--- gameplay.gd
+++ gameplay.gd
@@ -2,8 +2,13 @@ extends Node
 
 @export var combat_state : CombatState
 
+@onready var level_music = $LevelMusic
+
 func _ready() -> void:
 	new_game()
 
 func new_game():
 	combat_state.enemies_died = 0
+
+func start_level_music():
+	level_music.play()
---
+++ ui/level_start_ui.gd
@@ -0,0 +1,21 @@
+class_name LevelStartsUI
+extends Control
+
+@onready var label = $RichTextLabel
+@onready var intro_sfx = $IntroSFX
+
+func _ready():
+	get_tree().paused = true
+	
+	label.text = "[center][outline_size=8][outline_color=black][color=#ff1a1a]LEVEL 1[/color][/outline_color][/outline_size]\nDead Sector Facility[/center]"
+	
+	intro_sfx.play()
+
+func _on_start_button_pressed():
+	intro_sfx.stop()
+
+	get_tree().paused = false
+	
+	get_tree().get_first_node_in_group("gameplay").start_level_music()
+	
+	queue_free()
