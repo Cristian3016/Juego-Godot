fa6063107bb86bdf0d82f3c340717593cc0596fa
Cristian3016 <cristiancaro163@gmail.com>
2026-03-09 15:57:41 -0300

    Mejorandoel nivel 1

--- weapons/weapon_stats.gd
+++ weapons/weapon_stats.gd
@@ -1,6 +1,6 @@
 class_name WeaponStats
 extends Resource
 
-@export var damage : int = 10 
+@export var damage : int = 20
 @export var knockback_stats : KnockbackStats
 @export var sound : AudioStream
