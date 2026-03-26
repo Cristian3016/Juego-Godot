997e3e866f8b4c0c916e93f752be6fd91489b9c6
Cristian3016 <cristiancaro163@gmail.com>
2026-03-26 18:56:32 -0300

    Arreglando Cagadas

--- objects/characters/enemies/invader.gd
+++ objects/characters/enemies/invader.gd
@@ -1,4 +1,4 @@
-class_name Invader
+class_name Invaderd
 extends PlatformerCharacter2D
 
 @export var projectile_scene : PackedScene
@@ -9,48 +9,31 @@ func _physics_process(delta):
 
 	super._physics_process(delta)
 
-	if player_detected and is_instance_valid(player):
+	if player_detected:
 		velocity.x = 0
 		
-		var dir = sign(player.global_position.x - global_position.x)
-		if dir != 0:
+		if player:
+			var dir = sign(player.global_position.x - global_position.x)
 			scale.x = dir
 
 func _on_area_2d_body_entered(body) -> void:
 	if body.is_in_group("player"):
 		player = body
 		player_detected = true
-
-		var timer = get_node_or_null("ShootTimer")
-		if timer:
-			timer.start()
+		$ShootTimer.start()
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		player_detected = false
 		player = null
-
-		var timer = get_node_or_null("ShootTimer")
-		if timer:
-			timer.stop()
+		$ShootTimer.stop()
 	
 func _on_shoot_timer_timeout() -> void:
 
-	if not is_instance_valid(self):
-		return
-
-	if not is_instance_valid(player) or not projectile_scene:
-		return
-
-	var parent = get_parent()
-	if not is_instance_valid(parent):
-		return
-
-	var projectile = projectile_scene.instantiate()
-	projectile.shooter = self
-	projectile.global_position = $ShootPoint.global_position
-	
-	var dir = (player.global_position - $ShootPoint.global_position).normalized()
-	projectile.launch(dir)
-	
-	parent.add_child(projectile)
+	if player and projectile_scene:
+			var projectile = projectile_scene.instantiate()
+			projectile.shooter = self
+			projectile.global_position = $ShootPoint.global_position
+			var dir = (player.global_position - $ShootPoint.global_position).normalized()
+			projectile.launch(dir)
+			get_tree().current_scene.add_child(projectile)
